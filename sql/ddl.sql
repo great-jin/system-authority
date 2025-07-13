ea1e838debb21eb839affb2ac8e4aeaac084bb31
great <great__jin@163.com>
2025-07-13 18:28:11 +0800

    feat: system data

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,8 +1,8 @@
 -- auth_system.tb_store definition
 CREATE TABLE `tb_store`
 (
-    `store_id`   bigint(20) DEFAULT NULL,
-    `store_name` varchar(100) DEFAULT NULL
+    `store_id`   bigint(20) NOT NULL,
+    `store_name` varchar(100) NOT NULL
 );
 
 
@@ -10,9 +10,9 @@ CREATE TABLE `tb_store`
 CREATE TABLE `tb_order`
 (
     `order_id`   bigint(20) NOT NULL AUTO_INCREMENT,
-    `store_id`   bigint(20) DEFAULT NULL
-    `product_name` varchar(100) DEFAULT NULL,
-    `order_time` datetime DEFAULT NULL,
+    `store_id`   bigint(20) NOT NULL
+    `product_name` varchar(100) NOT NULL,
+    `order_time` datetime DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`order_id`)
 );
 
@@ -21,8 +21,8 @@ CREATE TABLE `tb_order`
 CREATE TABLE `tb_user`
 (
     `user_id`  bigint(20) NOT NULL AUTO_INCREMENT,
-    `username` varchar(100) DEFAULT NULL,
-    `password` varchar(100) DEFAULT NULL,
+    `username` varchar(100) NOT NULL,
+    `password` varchar(100) NOT NULL,
     PRIMARY KEY (`user_id`)
 );
 
@@ -31,7 +31,7 @@ CREATE TABLE `tb_user`
 CREATE TABLE `tb_role`
 (
     `role_id`   bigint(20) NOT NULL AUTO_INCREMENT,
-    `role_name` varchar(100) DEFAULT NULL,
+    `role_name` varchar(100) NOT NULL,
     PRIMARY KEY (`role_id`)
 ) DEFAULT CHARSET=utf8mb4;
 
@@ -39,30 +39,30 @@ CREATE TABLE `tb_role`
 -- auth_system.tb_user_role definition
 CREATE TABLE `tb_user_role`
 (
-    `user_id` bigint(20) DEFAULT NULL,
-    `role_id` bigint(20) DEFAULT NULL
+    `user_id` bigint(20) NOT NULL,
+    `role_id` bigint(20) NOT NULL
 );
 
 
 -- auth_system.tb_user_store definition
 CREATE TABLE `tb_user_store`
 (
-    `user_id`  bigint(20) DEFAULT NULL,
-    `store_id` bigint(20) DEFAULT NULL
+    `user_id`  bigint(20) NOT NULL,
+    `store_id` bigint(20) NOT NULL
 );
 
 
 -- auth_system.tb_role_menu definition
 CREATE TABLE `tb_role_menu`
 (
-    `role_id`  bigint(20) DEFAULT NULL,
-    `menu_key` varchar(19) DEFAULT NULL
+    `role_id`  bigint(20) NOT NULL,
+    `menu_key` varchar(19) NOT NULL
 );
 
 
 -- auth_system.tb_role_store definition
 CREATE TABLE `tb_role_store`
 (
-    `role_id`  bigint(20) DEFAULT NULL,
-    `store_id` bigint(20) DEFAULT NULL
+    `role_id`  bigint(20) NOT NULL,
+    `store_id` bigint(20) NOT NULL
 );
--- sql/dml.sql
+++ sql/dml.sql
@@ -1,14 +1,23 @@
-INSERT INTO auth_system.tb_user (user_id, username, password)
-VALUES (1, 'alex', '123456');
+INSERT INTO tb_user (user_id, username, password)
+VALUES (1, 'alex', '123456'),
+       (2, 'beth', '123456');
 
 
-INSERT INTO auth_system.tb_role (role_id, role_name)
+INSERT INTO tb_role (role_id, role_name)
 VALUES (1, 'admin');
 
 
-INSERT INTO auth_system.tb_role_menu (role_id, menu_key)
+INSERT INTO tb_user_role (user_id, role_id)
+VALUES (1, 1);
+
+
+INSERT INTO tb_role_menu (role_id, menu_key)
 VALUES (1, 'store.query'),
        (1, 'order.query');
+
+
+INSERT INTO tb_role_store (role_id, store_id)
+VALUES (1, 1);
 
 
 INSERT INTO tb_store (store_id, store_name)
@@ -16,5 +25,6 @@ VALUES (1, 'AA'),
        (2, 'BB');
 
 
-INSERT INTO auth_system.tb_role_store (role_id, store_id)
-VALUES (1, 1);
+INSERT INTO tb_order (order_id, store_id, product_name, order_time)
+VALUES (1, 1, 'sku1', now()),
+       (2, 2, 'sku2', now());
